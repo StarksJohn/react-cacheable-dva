13dad1f824c6f105fc6ab22f356b7ed1f21117e4
cham1985 <397866153@qq.com>
2021-09-21 01:18:58 +0800

    no message

--- index.ts
+++ index.ts
@@ -3,10 +3,11 @@ import baseModel from './baseModel'
 import models from './models'
 import { modelProps as _modelProps } from './modelProps'
 import dvaApp from './dvaApp'
+import tool from './tool'
 
 export interface modelProps extends _modelProps{
 }
 
 export {
-  dva, baseModel, models, dvaApp
+  dva, baseModel, models, dvaApp,tool
 }
